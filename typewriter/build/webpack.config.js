3e340025551b745e03a9f3b74104fe9b66023a71
Sudol <sudol@SudoldeMacBook-Pro.local>
2019-11-18 18:01:07 +0800

    react typescript

---
+++ typewriter/build/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: path.join(__dirname, '../src/index.tsx'),
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, '../dist')
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.css', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+        include: path.join(__dirname, '../src')
+      },
+      {
+        test: /\.tsx?$/,
+        use: ['babel-loader'],
+        include: [path.join(__dirname, '../src')],
+        exclude: /node-modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+        exclude: /node_modules/
+     }    
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'public/index.html',
+      inject: true
+    })
+  ],
+  devServer: {
+    host: 'localhost',
+    port: 3000,
+    historyApiFallback: true,
+    overlay: {
+      errors: true
+    },
+    inline: true,
+    hot: true
+  },
+}
